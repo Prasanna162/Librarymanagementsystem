b3eebe0fd253d483740a88ccc85fd16b9fd63997
Prasanna162 <prasannababu162@gmail.com>
2025-09-16 22:21:45 +0530

    Initial commit: Librarymanagementsystem

---
+++ src/CLIENT/about/aboutusData.js
@@ -0,0 +1,22 @@
+export const about_data = [
+  {
+    id: 1,
+    title: `Our Story`,
+    description: `We built this Library Management System to make borrowing, returning, and tracking books simple and efficient for students, teachers, and administrators.`
+,
+  },
+  {
+    id: 2,
+    title: `Our Mission`,
+    description: `Our mission is to provide easy access to knowledge by digitalizing the library experience. 
+    We aim to save time, reduce manual effort, and help readers discover books effortlessly through smart recommendations.`,
+  },
+  {
+    id: 3,
+    title: `Our Team`,
+    description: `This project was developed by a passionate team of learners and developers:
+    • Prasanna B
+    • Rahul R
+    • Prithiv Narayan B.`,
+  },
+]
